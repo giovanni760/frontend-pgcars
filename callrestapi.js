6930b99a20c4cc6ce1bc7a6f27d0fa2911692539
giovanni760 <giovanni162005@gmail.com>
2025-04-19 01:20:29 -0600

    update callrestapi

--- callrestapi.js
+++ callrestapi.js
@@ -1,5 +1,5 @@
-var url = "http://localhost:8080/api/car";
-//var url = "https://mysql-apicars.onrender.com/api/car";
+//var url = "http://localhost:8080/api/car";
+var url = "https://pg-autos.onrender.com/api/car";
 
 function postCar() {
 
